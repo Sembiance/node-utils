893a2971adefdcf04dba451841d1fa5139dd48ff
Robert Schultz <robert@cosmicrealms.com>
2013-02-14 09:17:13 -0500

    Added an options map for dust render, and an option to keep whitespace

--- dust-utils.js
+++ dust-utils.js
@@ -6,8 +6,16 @@ var base = require("node-base"),
 	fs = require("fs");
 
 exports.render = render;
-function render(basePath, name, data, cb)
+function render(basePath, name, data, options, cb)
 {
+	if(!cb)
+	{
+		cb = options;
+		options = null;
+	}
+
+	options = options || {};
+
 	var readDustFile = function(name, cb) { fs.readFile(path.join(basePath, name + (name.endsWith(".dust") ? "" : ".dust")), "utf8", cb); };
 
 	var dust = require("dustjs-linkedin");
@@ -23,6 +31,7 @@ function render(basePath, name, data, cb)
 			if(err)
 				throw err;
 
+			dust.optimizers.format = function(ctx, node) { if(options.keepWhitespace) { return node; } };
 			dust.renderSource(template, data, this);
 		},
 		function returnResult(err, result) { cb(err, result); }
