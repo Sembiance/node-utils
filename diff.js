dce73098f5da8e395332f0ebf0df2ee2eba45c8d
Robert Schultz <robert@cosmicrealms.com>
2015-06-21 15:16:57 -0400

    Added support for comparing arrays directly

--- diff.js
+++ diff.js
@@ -6,19 +6,20 @@ var base = require("xbase"),
 	color = require("cli-color");
 
 exports.diff = diff;
-function diff(o, n, indent)
+function diff(o, n, options)
 {
-	indent = (indent || 0) + 1;
+	options = base.clone((options || {}));
+	options.indent = (options.indent || 0) + 1;
 
 	if(Object.isObject(o))
-		return diffObjects(o, n, indent);
+		return diffObjects(o, n, options);
 	else if(Array.isArray(o))
-		return diffArray(o, n, indent);
+		return diffArray(o, n, options);
 	else
-		return diffValues(o, n);
+		return diffValues(o, n, options);
 }
 
-function diffObjects(o, n, indent)
+function diffObjects(o, n, options)
 {
 	var result = "";
 
@@ -26,32 +27,51 @@ function diffObjects(o, n, indent)
 	var nKeys = Object.keys(n);
 
 	var keysAdded = nKeys.subtract(oKeys);
-	keysAdded.forEach(function(keyAdded) { result += " ".repeat(indent*4) + color.green(util.format("%s : %j\n", keyAdded, n[keyAdded])); });
+	keysAdded.forEach(function(keyAdded) { result += " ".repeat(options.indent*4) + color.green(util.format("%s : %j\n", keyAdded, n[keyAdded])); });
 
 	var keysRemoved = oKeys.subtract(nKeys);
-	keysRemoved.forEach(function(keyRemoved) { result += " ".repeat(indent*4) + color.red(util.format("%s : %j\n", keyRemoved, o[keyRemoved])); });
+	keysRemoved.forEach(function(keyRemoved) { result += " ".repeat(options.indent*4) + color.red(util.format("%s : %j\n", keyRemoved, o[keyRemoved])); });
 
 	oKeys.subtract(keysAdded).subtract(keysRemoved).forEach(function(key)
 	{
-		var subResult = diff(o[key], n[key], indent);
+		var subResult = diff(o[key], n[key], options);
 		if(subResult)
-			result += " ".repeat(indent*4) + color.yellow(key) + color.white(" : ") + subResult;
+			result += " ".repeat(options.indent*4) + color.yellow(key) + color.white(" : ") + subResult;
 	});
 
 	return (result.length ? "{\n" : "") + result + (result.length ? "}\n" : "");
 }
 
-function diffArray(o, n, indent)
+function diffArray(o, n, options)
 {
 	var result = "";
 
-	n.map(function(v) { return JSON.stringify(v); }).subtract(o.map(function(v) { return JSON.stringify(v); })).forEach(function(added) { result += (result.length ? ", " : "") + color.green(added); });
-	o.map(function(v) { return JSON.stringify(v); }).subtract(n.map(function(v) { return JSON.stringify(v); })).forEach(function(removed) { result += (result.length ? ", " : "") + color.red(removed); });
+	if(options.compareArraysDirectly)
+	{
+		if(o.length!==n.length)
+		{
+			result += "Arrays are not equal length, cannot compare them directly: old [" + o.length + "] vs new [" + n.length + "]";	
+		}
+		else
+		{
+			o.forEach(function(item, i)
+			{
+				var subResult = diff(item, n[i], options);
+				if(subResult)
+					result += " ".repeat(options.indent*4) + "[" + i + "]: " + subResult;
+			});
+		}
+	}
+	else
+	{
+		n.map(function(v) { return JSON.stringify(v); }).subtract(o.map(function(v) { return JSON.stringify(v); })).forEach(function(added) { result += (result.length ? ", " : "") + color.green(added); });
+		o.map(function(v) { return JSON.stringify(v); }).subtract(n.map(function(v) { return JSON.stringify(v); })).forEach(function(removed) { result += (result.length ? ", " : "") + color.red(removed); });
+	}
 
 	return (result.length ? "[ " : "") + result + (result.length ? " ]\n" : "");
 }
 
-function diffValues(o, n)
+function diffValues(o, n, options)
 {
 	if(o!==n)
 	{
