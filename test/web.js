cbd5169b58e464a137ac7aa7426ddd64993cb826
Robert Schultz <robert@cosmicrealms.com>
2018-12-31 15:18:08 -0500

    Added test for web screenshot util

---
+++ test/web.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const path = require("path"),
+	fs = require("fs"),
+	fileUtil = require("../index").file,
+	webUtil = require("../index").web;
+
+const SCREENSHOT_PATH = path.join(__dirname, "web-screenshot.png");
+
+if(fileUtil.existsSync(SCREENSHOT_PATH))
+	fs.unlinkSync(SCREENSHOT_PATH);
+
+webUtil.captureScreenshot("http://worldofsolitaire.com", {width : 1024, height : 768, delay : 2000}, (err, ss) =>
+{
+	if(err)
+	{
+		console.error("Run first: chromium --headless --hide-scrollbars --remote-debugging-port=9222 --disable-gpu");
+		console.error(err);
+		process.exit(1);
+	}
+
+	fs.writeFile(SCREENSHOT_PATH, ss, () =>
+	{
+		console.log("Examine %s to see if it worked, should be a screenshot of worldofsolitaire.com", path.basename(SCREENSHOT_PATH));
+		process.exit(0);
+	});
+});
