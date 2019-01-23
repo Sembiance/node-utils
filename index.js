dbcb89a1c4782620b574836d4eedf3c6603db38c
Robert Schultz <robert@cosmicrealms.com>
2019-01-23 13:16:48 -0500

    Remove erbridge from xutil

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ exports.run = require("./runUtil.js");
 exports.file = require("./fileUtil.js");
 exports.dust = require("./dustUtil.js");
 exports.unicode = require("./unicodeUtil.js");
-exports.erbridge = require("./erbridgeUtil.js");
 exports.crypto = require("./cryptoUtil.js");
 exports.print = require("./printUtil.js");
 exports.video = require("./videoUtil.js");
