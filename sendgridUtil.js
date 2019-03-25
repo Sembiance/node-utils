86fd090e23c67b33959cf2de6d8ccb13a989ff60
Robert Schultz <robert@cosmicrealms.com>
2019-03-25 15:24:16 -0400

    Updated to latest sendgrid client module

--- sendgridUtil.js
+++ sendgridUtil.js
@@ -1,18 +1,10 @@
 "use strict";
 
 const XU = require("@sembiance/xu"),
-	mailHelper = require("sendgrid").mail;
+	sgMail = require("@sendgrid/mail");
 
 exports.send = function send(apiKey, msg, cb)
 {
-	const mail = new mailHelper.Mail(new mailHelper.Email(msg.from), msg.subject, new mailHelper.Email(msg.to), new mailHelper.Content("text/plain", msg.text));
-
-	const sg = require("sendgrid")(apiKey);	// eslint-disable-line global-require
-	const request = sg.emptyRequest({
-		method : "POST",
-		path   : "/v3/mail/send",
-		body   : mail.toJSON()
-	});
-
-	sg.API(request, cb);	// eslint-disable-line new-cap
+	sgMail.setApiKey(apiKey);
+	sgMail.send(msg, cb);
 };
---
+++ test/sendgrid.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const XU = require("@sembiance/xu"),
+	sendgridUtil = require("../index").sendgrid;
+
+if(process.argv.length<3)
+{
+	console.log("Usage: node sendgrid.js <sendGrid API Key>");
+	process.exit(1);
+}
+
+const code = Math.randomInt(0, 99999);
+
+sendgridUtil.send(process.argv[2], {
+	to      : "robert@cosmicrealms.com",
+	from    : "robert@telparia.com",
+	subject : "Test e-mail message " + code,
+	text    : "This is just a test to see if the e-mail sending works. Code: " + code
+}, () => { console.log("Check robert@cosmicrealms.com e-mail for code %d", code); process.exit(0); });
