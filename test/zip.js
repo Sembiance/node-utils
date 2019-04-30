df6e673163bdd187782ba1b79053d90ab80d0d17
Robert Schultz <robert@cosmicrealms.com>
2019-04-30 18:54:57 -0400

    Added a zipFiles method

--- test/zip.js
+++ test/zip.js
@@ -4,23 +4,83 @@ const XU = require("@sembiance/xu"),
 	path = require("path"),
 	fs = require("fs"),
 	assert = require("assert"),
+	tiptoe = require("tiptoe"),
+	fileUtil = require("../index").file,
 	zipUtil = require("../index").zip;
 
-zipUtil.readZipEntries(path.join(__dirname, "test.zip"), () => false, () => assert(!true), err => assert(!err));
-zipUtil.readZipEntries(path.join(__dirname, "test.zip"), entry => entry.fileName==="zip_hello.txt", (entry, entryData) => assert.strictEqual(entryData.toString("utf8"), "This is just a text file"), err => assert(!err));
-
-let seenZipEntry = false;
-let zipEntryData = null;
-zipUtil.readZipEntries(path.join(__dirname, "test.zip"), entry => entry.fileName==="subdir/zip_smile.png", (entry, entryData) =>
+function test_readZipEntries(zipFilePath, cb)	// eslint-disable-line camelcase
 {
-	seenZipEntry = true;
-	zipEntryData = entryData;
-	assert(entryData.equals(fs.readFileSync(path.join(__dirname, "zip_smile.png"))));
-}, (err, zipEntries) =>
-{
-	assert(!err);
-	assert(seenZipEntry);
-	assert.strictEqual(zipEntries.length, 1);
-	assert.strictEqual(zipEntryData, zipEntries[0]);
-});
+	tiptoe(
+		function tests()
+		{
+			zipUtil.readZipEntries(zipFilePath, () => false, () => assert(!true), this.parallel());
+			let seenEntry = false;
+			zipUtil.readZipEntries(zipFilePath, entry => entry.fileName==="zip_hello.txt", (entry, entryData) => { seenEntry = true; assert.strictEqual(entryData.toString("utf8"), "This is just a text file"); }, err =>
+			{
+				assert(!err);
+				assert.strictEqual(seenEntry, true);
+				this.parallel()();
+			});
+		},
+		function moreTests()
+		{
+			let seenZipEntry = false;
+			let zipEntryData = null;
+			zipUtil.readZipEntries(zipFilePath, entry => entry.fileName==="subdir/zip_smile.png", (entry, entryData) =>
+			{
+				seenZipEntry = true;
+				zipEntryData = entryData;
+				assert(entryData.equals(fs.readFileSync(path.join(__dirname, "zip_smile.png"))));
+			}, (err, zipEntries, zipEntriesData) =>
+			{
+				assert(!err);
+				assert(seenZipEntry);
+				assert.strictEqual(zipEntries[0].fileName, "subdir/zip_smile.png");
+				assert.strictEqual(zipEntriesData.length, 1);
+				assert.strictEqual(zipEntryData, zipEntriesData[0]);
 
+				setImmediate(this);
+			});
+		},
+		cb
+	);
+}
+
+tiptoe(
+	function testExistingZIPFile()
+	{
+		test_readZipEntries(path.join(__dirname, "test.zip"), this);
+	},
+	function createZipFile()
+	{
+		zipUtil.zipFiles(["pewpewpew.flac", "subdir", "zip_hello.txt"], "/tmp/zip_test_test.zip", {cwd : path.join(__dirname, "zip_dir")}, this.parallel());
+		zipUtil.zipFiles(["pewpewpew.flac", "subdir", "zip_hello.txt"], "/tmp/zip_test_test_renamed.zip", {junkPaths : true, renameMap : { "zip_hello.txt" : "renamed_hellow.txt" }, cwd : path.join(__dirname, "zip_dir")}, this.parallel());
+	},
+	function checkMadeZipFileAndRemove()
+	{
+		test_readZipEntries("/tmp/zip_test_test.zip", this.parallel());
+		let seenEntry = false;
+		zipUtil.readZipEntries("/tmp/zip_test_test_renamed.zip", entry => entry.fileName==="renamed_hellow.txt", (entry, entryData) => { seenEntry = true; assert.strictEqual(entryData.toString("utf8"), "This is just a text file"); }, err =>
+		{
+			assert(!err);
+			assert.strictEqual(seenEntry, true);
+			this.parallel()();
+		});
+	},
+	function sleepSome()
+	{
+		setTimeout(this, XU.SECOND*3);
+	},
+	function removeZipFile()
+	{
+		fileUtil.unlink("/tmp/zip_test_test.zip", this.parallel());
+		fileUtil.unlink("/tmp/zip_test_test_renamed.zip", this.parallel());
+	},
+	function handleErrors(err)
+	{
+		if(err)
+			console.error(err);
+
+		assert(!err);
+	}
+);
--- zipUtil.js
+++ zipUtil.js
@@ -2,12 +2,18 @@
 
 const XU = require("@sembiance/xu"),
 	tiptoe = require("tiptoe"),
+	fs = require("fs"),
+	fileUtil = require("./fileUtil.js"),
+	runUtil = require("./runUtil.js"),
+	path = require("path"),
 	yauzl = require("yauzl");
 
-// Will read the entries in the given zipFile (Buffer or string path) and call entryChecker(entry) and every time it returns true it will call fileHandler(entry, entryData) and call cb when all done
+// Will read the entries in the given zipFile (Buffer or string path) and call entryChecker(entry) and every time it returns true it will call fileHandler(entry, entryData) and call cb(err, allEntries) when all done
+// entryChecker and fileHandler can be null if you don't want to filter out entries or be called for ever file
 exports.readZipEntries = function readZipEntries(zipPath, entryChecker, fileHandler, cb)
 {
 	const zipEntries = [];
+	const zipEntriesData = [];
 
 	tiptoe(
 		function openZip()
@@ -19,7 +25,7 @@ exports.readZipEntries = function readZipEntries(zipPath, entryChecker, fileHand
 			zipFile.once("end", this);
 			zipFile.on("entry", entry =>
 			{
-				if(!entryChecker(entry))
+				if(typeof entryChecker==="function" && !entryChecker(entry))
 					return setImmediate(() => zipFile.readEntry());
 				
 				const chunks = [];
@@ -27,15 +33,18 @@ exports.readZipEntries = function readZipEntries(zipPath, entryChecker, fileHand
 				{
 					if(err)
 						return this(err);
+
+					zipEntries.push(entry);
 					
 					readStream.on("error", readStreamErr => this(readStreamErr));
 					readStream.on("data", chunk => chunks.push(chunk));
 					readStream.once("end", () =>
 					{
 						const entryData = Buffer.concat(chunks);
-						zipEntries.push(entryData);
+						zipEntriesData.push(entryData);
 
-						fileHandler(entry, entryData);
+						if(typeof fileHandler==="function")
+							fileHandler(entry, entryData);
 						zipFile.readEntry();
 					});
 					readStream.read(entry.uncompressedSize);
@@ -46,7 +55,67 @@ exports.readZipEntries = function readZipEntries(zipPath, entryChecker, fileHand
 		},
 		function returnResult(err)
 		{
-			cb(err, zipEntries);
+			cb(err, zipEntries, zipEntriesData);
 		}
+	);
+};
+
+// Will zip the given filePaths into zipPath
+// Can rename files on the fly with renameMap and can set the cwd in options as well
+exports.zipFiles = function zipFiles(_filePaths, zipPath, options, cb)
+{
+	if(!cb)
+	{
+		cb = options;	// eslint-disable-line no-param-reassign
+		options = { };	// eslint-disable-line no-param-reassign
+	}
+
+	const filePaths = _filePaths.slice();
+
+	tiptoe(
+		function createRenameDir()
+		{
+			if(!options.renameMap)
+				return this();
+
+			this.data.tmpRenameDir = fileUtil.generateTempFilePath();
+
+			fs.mkdir(this.data.tmpRenameDir, this);
+		},
+		function copyFiles()
+		{
+			if(!options.renameMap)
+				return this();
+			
+			filePaths.parallelForEach((filePath, subcb, i) =>
+			{
+				const renameTo = options.renameMap[path.basename(filePath)];
+				if(!renameTo)
+					return setImmediate(subcb);
+
+				const destFilePath = path.join(this.data.tmpRenameDir, renameTo);
+				filePaths[i] = destFilePath;
+				
+				fileUtil.copy(path.join((options.cwd || ""), filePath), destFilePath, subcb);
+			}, this);
+		},
+		function createZip()
+		{
+			const cwd = options.cwd || path.dirname(filePaths[0]);
+			const zipArgs = ["-r"];
+			if(options.junkPaths)
+				zipArgs.push("-j");
+			zipArgs.push(zipPath, ...filePaths);
+
+			runUtil.run("zip", zipArgs, { cwd, silent : true }, this);
+		},
+		function cleanupTmpDir()
+		{
+			if(!options.renameMap)
+				return this();
+
+			fileUtil.unlink(this.data.tmpRenameDir, this);
+		},
+		cb
 	);
 };
